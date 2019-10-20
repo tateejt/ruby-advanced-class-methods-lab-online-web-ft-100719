a40b93996ff91b8417e0b56da540b42ffbe5e09e
Emery Tate <tateejt@gmail.com>
2019-10-20 21:29:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
   
   def self.new_from_filename(name)
     song = self.new
-    song.name = (name.split("-")[1].chomp)
+    song.name = (name.split("-")[1].chomp(.mp3))
   end
   
 end
